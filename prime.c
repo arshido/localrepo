f0eb206635d5060b3d4b8c4cff8ec2e119ac60d5
Aryaman Singh <kishu956@gmail.com>
2025-03-22 11:50:03 +0530

    prime no. checker

--- prime.c
+++ prime.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 int main()
 {
-    printf("hello world");
+    int n;
+    printf("enter a no.: ");
+    scanf("%d",&n);
+    int c=count(n);
+    if(c==2)
+    {
+        printf("%d is a prime no.",n);
+    }
+    else
+    {
+        printf("%d is not a prime no.",n);
+    }
+    return 0;  
+}
+int count(int a)// count the no. of factors
+{
+    int c=0;
+    for (int i = 0; i <=a; i++)
+    {
+       if (a%i==0)
+       {
+        c++;
+       }
+    }
+    return c;
 }
